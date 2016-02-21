2c8a67520224ca1d8d37129fbc0bdf51f7bf8b6e
GsDhillon <Gursewak_singh36@yahoo.com>
2016-02-20 21:18:10 -0800

    Added data to Sales data

--- MyHub/src/SaleData.java
+++ MyHub/src/SaleData.java
@@ -1,7 +1,13 @@
 
 public class SaleData {
 	public static void display(){
-		System.out.println("I am in the Sales class in display methhod!");
+		System.out.println("Data");
+		int data[]={1,23,25};
+		for(int i=0;i<data.length;i++)
+		{
+			System.out.println(data[i]);
+			
+		}
     }
 
 }
