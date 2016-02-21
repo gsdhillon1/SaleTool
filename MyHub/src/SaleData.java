299fffc896318f2ab02a366b4f4bda7798cabbc8
GsDhillon <Gursewak_singh36@yahoo.com>
2016-02-21 13:56:20 -0800

    New sales are added by Nathan!

--- MyHub/src/SaleData.java
+++ MyHub/src/SaleData.java
@@ -1,8 +1,9 @@
 
 public class SaleData {
 	public static void display(){
+		System.out.println("This sale is modified by Nathan recently!");
 		System.out.println("Data");
-		int data[]={1,23,25};
+		int data[]={1,23,25,56,78,89,100};
 		for(int i=0;i<data.length;i++)
 		{
 			System.out.println(data[i]);
