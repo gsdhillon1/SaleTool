eddbfc18cfdaa038bfeed14dfa45a1d417cefc9d
GsDhillon <Gursewak_singh36@yahoo.com>
2016-02-21 15:28:59 -0800

    Developer Dhillon added the sum and tax method in sales class!

--- MyHub/src/SaleData.java
+++ MyHub/src/SaleData.java
@@ -4,11 +4,14 @@ public class SaleData {
 		System.out.println("This sale is modified by Nathan recently!");
 		System.out.println("Data");
 		int data[]={1,23,25,30,45,50};
-		for(int i=0;i<data.length;i++)
+		int sum=0;
+		int tax=5;
+		for(int i: data)
 		{
-			System.out.println(data[i]);
-			
+			System.out.println(i);
+			sum=sum+(i*tax);
 		}
+		System.out.println("The total accumalated sum is->"+sum);
     }
 
 }
